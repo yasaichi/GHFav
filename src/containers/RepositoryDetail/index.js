f947f25146c1a781390a681ac4c218b6bd7399b2
yasaichi <yasaichi@users.noreply.github.com>
2017-05-01 16:32:29 +0900

    Uninstall RNRF and remove the related files

--- src/containers/RepositoryDetail/index.js
+++
@@ -1,7 +0,0 @@
-// @flow
-import { connect } from 'react-redux';
-
-import RepositoryDetail from '../../components/RepositoryDetail';
-import mapStateToProps from './mapStateToProps';
-
-export default connect(mapStateToProps)(RepositoryDetail);
