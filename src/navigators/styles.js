1bed753482136c4f499b4c2fda761cd66c7bf277
yasaichi <yasaichi@users.noreply.github.com>
2017-05-01 15:52:15 +0900

    Implement navigators with react-navigation

---
+++ src/navigators/styles.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  card: {
+    backgroundColor: 'white',
+  },
+  header: {
+    backgroundColor: '#24292e',
+  },
+  headerTitle: {
+    color: 'white',
+    fontWeight: 'bold',
+    width: 240,
+  },
+});
